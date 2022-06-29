91f337d3c4e6ec20d89f5eef5b78abffe4fbc31e
huseyni <huseyinozgurkazan@gmail.com>
2022-06-29 14:23:23 +0300

    16 Ocak Ders Sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi.");
+        }
+
+    }
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Ad = "Huseyin";
+musteri1.Soyad = "Kazan";
+musteri1.Adres = "Ankara";
+
+MusteriManager musteriManager = new MusteriManager();
+
+musteriManager.Ekle(musteri1);
+musteriManager.Sil(musteri1);
+
