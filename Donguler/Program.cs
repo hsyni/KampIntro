9fc9f3442d180d5e2882a75a062923bd051d134b
huseyni <huseyinozgurkazan@gmail.com>
2022-06-29 10:48:43 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,15 @@
+﻿//array-dizi
+
+string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlşanguç için temel kurs", "Java", "Python"};
+
+for (int i = 0; i < kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+}
+
+foreach (string kurs in kurslar)
+{
+    Console.WriteLine(kurs);
+}
+
+Console.WriteLine("Footer");
